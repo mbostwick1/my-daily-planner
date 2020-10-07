81979a38d27d7013d1bce01a1bd4efd4975fdb29
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-06 20:01:28 -0400

    current date added

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    console.log("oh hey");
+    // console.log("oh hey");
+    //Date counter//
+    $("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do"));
+    //DOM Variables
+
+
+    //JS Variables
+
+
+    //Function Definitions
+
+
+    //Function Calls
+
+
+    //Event Listeners
+
+
+
 });
 
 
