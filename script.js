402f8a4989675b47e73c325b2fe626916a822528
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-06 21:50:08 -0400

    Make Plans function to save description to local storage

--- script.js
+++ script.js
@@ -9,31 +9,45 @@ $(document).ready(function () {
   var block = $(".time-block");
   var saveBtn = $(".saveBtn");
 
-  //Function Calls
+// Function Calls //
+  // Make Plans //
+  function makePlans() {
+    block.each(function () {
+        var id = $(this).attr("id");
+        var plans = localStorage.getItem(id);
+
+        if (plans !== null) {
+            $(this).children(".description").val(plans);
+        }
+    });
+
+  }
+  makePlans();
+
   //Function to determine the hour and past, present, future css styling //
   function hourDisplay() {
     //get current hour
     var currentTime = moment().hour();
     $(block).each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-        // console.log(currentTime, blockTime);
-        if (currentTime === blockTime) {
-          $(this).addClass("present");
-        } else if (currentTime > blockTime) {
-          $(this).addClass("past");
-        } else {
-          $(this).addClass("future");
-        }
-      });
-   
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+      // console.log(currentTime, blockTime);
+      if (currentTime === blockTime) {
+        $(this).addClass("present");
+      } else if (currentTime > blockTime) {
+        $(this).addClass("past");
+      } else {
+        $(this).addClass("future");
+      }
+    });
   }
   hourDisplay();
 
-  function makePlans() {}
+ 
 
   //Event Listeners
   saveBtn.on("click", function () {
     //  console.log("button clicked");
     // console.log(this);
+
   });
 });
