0ccbc4eef8283d0f23470ad0bde7aa2c8751a788
Melanie Bostwick <mbostwick1@gmail.com>
2020-10-06 20:54:04 -0400

    save button and changed IDs for the sake of the hour display function

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ $(document).ready(function () {
     // console.log("oh hey");
     //Date counter//
     $("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do"));
-    //DOM Variables
+   
+    // variables 
+  
 
-
-    //JS Variables
-
-
+    var saveBtn = $(".saveBtn");
     //Function Definitions
 
 
     //Function Calls
+    //Function to determine the hour and past, present, future css styling //
+    function hourDisplay() {
+        //get current hour
+        var currentHour = moment().hour();
+        
+       
 
+    }
+
+    function makePlans() {
+
+
+
+    }
 
     //Event Listeners
-
+    saveBtn.on("click", function () {
+        //  console.log("button clicked");
+        // console.log(this);
+    })
 
 
 });
